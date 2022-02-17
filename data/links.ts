f8b3ddf43b278aa327c893a01b7845578a2cf2aa
ferdous-al-quraeshi <ecopavel@gmail.com>
2022-02-17 16:40:11 +0600

    Building the GraphQL API - usding ApolloServer and resolvers with static data

--- data/links.ts
+++ data/links.ts
@@ -21,7 +21,7 @@ export const links = [
     description: "Utility-fist css framework",
     id: "6a9122b2-363b-4a4f-ad26-d6c55b51baed",
     imageUrl:
-      "https://tailwindcss.com/_next/static/media/twitter-large-card.85c0ff9e455da585949ff0aa50981857.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh6bDx-PkyPPJ8WNfOsNgpIsYb1-8JFFT5Pg&usqp=CAU",
     title: "TailwindCSS",
     url: "https://tailwindcss.com",
   },
---
+++ pages/api/graphql.ts
@@ -0,0 +1,38 @@
+/*
+  Leverage Next.js' API routes
+*/
+
+import { ApolloServer } from 'apollo-server-micro'
+import { typeDefs } from '../../graphql/schema'
+import { resolvers } from '../../graphql/resolvers'
+import Cors from 'micro-cors'
+
+const cors = Cors()
+
+/*
+  Instantiation of ApolloServer with defined schema and resolvers
+*/
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+const startServer = apolloServer.start()  // This is a requirement of Apollo Server 3
+
+export default cors(async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+  await startServer
+
+  await apolloServer.createHandler({
+    path: '/api/graphql',  // endpoint of the graphql api
+  })(req, res)
+})
+
+/*
+  The 'config' object facilitates making changes to the default configs
+*/
+export const config = {
+  api: {
+    bodyParser: false,  // Body parsing is disabled here since it is handled by GraphQL
+  },
+}
