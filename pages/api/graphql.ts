10528204f2db2ba8d9ad48e42b1c0a4c67984e78
ferdous-al-quraeshi <ecopavel@gmail.com>
2022-02-18 12:52:48 +0600

    Building the GraphQL API (continued...) - creating schema using nexus - defining schema and queries using nexus

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -3,7 +3,7 @@
 */
 
 import { ApolloServer } from 'apollo-server-micro'
-import { typeDefs } from '../../graphql/schema'
+import { schema } from '../../graphql/schema'
 import { resolvers } from '../../graphql/resolvers'
 import { createContext } from '../../graphql/context'
 import Cors from 'micro-cors'
@@ -14,7 +14,7 @@ const cors = Cors()
   Instantiation of ApolloServer with defined schema and resolvers
 */
 const apolloServer = new ApolloServer({
-  typeDefs,
+  schema,
   resolvers,
   context: createContext
 })
